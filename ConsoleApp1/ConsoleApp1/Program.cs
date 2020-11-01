d75f7d49af6c42a45bbd5c8bff611ecf0bd23098
Bondar Vladislav <72622783+Bondar-Vladislav@users.noreply.github.com>
2020-11-01 20:01:47 +0600

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        [DllImport(@"C:\Users\3000v\OneDrive\Рабочий стол\Lab2.1\Lib2-1.dll", 
+            CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern double TheFunc(string name, double x);
+
+        static void Main(string[] args)
+        {
+            new Form1().ShowDialog();
+        }
+    }
+}
